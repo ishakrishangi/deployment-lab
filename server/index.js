65356d2a4d4683a3771a71453167a30fd43a85e5
ishakrishangi <krishangik@gmail.com>
2022-07-28 15:29:46 -0400

    made some changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
-
 const app = express()
+app.use(express.json())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
 app.use(express.static(path.join(__dirname, "/../public"))) 
