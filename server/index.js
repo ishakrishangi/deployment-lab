da7774612b6687b22c1f8fc5ef754d95524cfa92
ishakrishangi <krishangik@gmail.com>
2022-07-28 15:59:48 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,61 @@ var rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 
+//rollbar user access
+app.get('/', (req, res) => {
+    rollbar.info(`user accessed the main page`)
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/api/students', (req, res) => {
+    rollbar.info('somebody got the student list')
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           rollbar.log(`user ${name} created successfully`)
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.warning(`no name was given to add`)
+           res.status(400).send('You must enter a name.')
+       } else {
+            rollbar.critical(`user tried adding a name that already exists`)
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+    rollbar.error(err)
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    rollbar.warning(`student deleted`)
+    res.status(200).send(students)
+})
+
+
+
+
+
+
+
+
+
+
 app.use(express.static(path.join(__dirname, "/../public"))) 
 
 app.get('/', function(req,res) {
